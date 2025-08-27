a729b8bf194751cb5249c2533d08fd2141d242a9
notlikelion <notlikelion@gmail.com>
2025-08-27 10:40:27 +0900

    죄송합니다

--- src/main/java/org/example/SpringCore.java
+++ src/main/java/org/example/SpringCore.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringCore {
     public static void main(String[] args) {
         System.out.println("----- 스프링 컨테이너 초기화 시작 -----");
 
         // context ? -> 앱 설정. AppConfig? SpringConfig?
         // Bean...
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
+            System.out.println("----- 스프링 컨테이너 초기화 완료 -----");
+            System.out.println();
+        }
+
     }
 }
